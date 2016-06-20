f8b141b70de31b713c77e202d6fb31f73a329f48
sp <281317914@qq.com>
2016-06-20 08:27:17 +0800

    qq

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fortune = require('../lib/mytest/fortune');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/about',function(req,res){
+  res.json({name:fortune.getFortune()});
+});
 module.exports = router;
