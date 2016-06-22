cf11a3aeb8cb862b3c3f04b4824594506ffc0965
sp <281317914@qq.com>
2016-06-22 08:30:21 +0800

    qq

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('home');
 });
-router.get('/about',function(req,res){
-  res.json({name:fortune.getFortune()});
+router.get('/myform', function(req, res, next) {
+  res.render('myform');
 });
-router.get('/error',function(req,res){
-  res.status(500);
-  res.render('error');
-})
+router.post('/formdata', function(req, res, next) {
+  res.json(req.body)
+});
+
+
+
+
+// app.get('/', function (req, res) {
+//   res.render('home');
+// });
+// router.get('/about',function(req,res){
+//   res.json({name:fortune.getFortune()});
+// });
+// router.get('/error',function(req,res){
+//   res.status(500);
+//   res.render('error');
+// })
 module.exports = router;
