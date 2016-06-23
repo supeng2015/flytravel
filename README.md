7fab134fb19850c1490a29f364aa2e9664953092
sp <281317914@qq.com>
2016-06-23 11:41:41 +0800

    qqq

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # flytravel
 my first web follow by express book
+我的无主题web
+
+技术
+angular  ：接口获取api
+bootstrop ： 前端布局
+bower ： 库加载
+grunt : 跑前端代码
+node ： 提供api,后端模版
+express : 后端框架 
+handlebars ： 后端模版引擎
+nigix : 存放静态文件
+mongoldb ：数据库
+
+路径
+cd documents/angular/ancient-  前端路径
+cd documents/express/flytravel   后端路径
+./bin/www   启动
+cd documents/mongodb/bin   数据库命令路径
+./mongod --dbpath=../data/db  启动数据库命令
+./mongo 启动数据库shell
+
+ show dbs  显示数据库
+
---
+++ model/catdata.js
@@ -0,0 +1,22 @@
+var mongoose = require('./db.js');
+//建一个草图
+var kittySchema = mongoose.Schema({
+  name: String
+});
+kittySchema.methods.speak = function () {
+  var greeting = this.name ? "Meow name is " + this.name : "I don't have a name"
+  console.log(greeting);
+}
+//模型化草图
+var Kitten = mongoose.model('Kitten', kittySchema);
+//实例化一条数据
+var silence = new Kitten({ name: 'Silence' })
+// silence.save(function (err, silence) {
+//   if (err) return console.error(err);
+//   silence.speak();
+// });
+Kitten.find(function (err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens)
+})
+module.exports = Kitten;
