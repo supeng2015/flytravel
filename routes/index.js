15fab58760dc2fa79a3c60ed6140afc5f918eda9
sp <281317914@qq.com>
2016-06-28 08:43:15 +0800

    qq

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home');
+ 
+  //console.log(req);
+  res.render('home',{
+    ip : req.ip	
+  });
 });
 router.get('/myform', function(req, res, next) {
   res.render('myform');
