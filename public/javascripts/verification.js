51d46c0b3ca093438a5f7132a6bef5e479120e0c
sp <281317914@qq.com>
2016-06-29 08:53:33 +0800

    register

--- public/javascripts/verification.js
+++ public/javascripts/verification.js
@@ -1,6 +1,38 @@
 sp.verification = function(){
-  return {	
-    surePassword : function(){ 	
+  return {
+    legal : true,
+    //空验证
+    emptyCom : function(){
+      var bre = false;
+      $.each(arguments,function(i,n){
+        if(!n.val()&&!bre){
+          alert('请填写完整');
+          bre = true;    
+        }
+      })
+      if(bre){
+        this.legal = false;   
+      }
+    },
+    //确认密码
+    surePassword : function(pass,cpass){
+     	console.log(pass);
+      if(pass.val()!=cpass.val()){
+      	alert('确认密码不一致');
+        this.legal = false;       
+      }    
+    },
+    //提交验证
+    submitForm : function(formid,pass,cpass,name,email){
+      formid.submit(function(){
+        $that = sp.verification();
+        $that.emptyCom(pass,cpass,name,email);
+        $that.surePassword(pass,cpass);
+        console.log($that.legal);
+        if(!$that.legal){
+          return false	
+        }
+      });	
     }
   }
 }
